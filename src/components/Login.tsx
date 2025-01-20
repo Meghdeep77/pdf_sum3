a033a299a371fd7df00a9259892e6df3b3c5e788
Meghdeep77 <117975443+Meghdeep77@users.noreply.github.com>
2025-01-20 18:15:47 +0530

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -22,7 +22,7 @@ const Login: React.FC = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:8080/auth/login", {
+      const response = await fetch("https://defmogu.in/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
