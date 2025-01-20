f55d45a17b78aab550d920747434769efd14bd2a
Meghdeep77 <117975443+Meghdeep77@users.noreply.github.com>
2025-01-20 18:17:11 +0530

    Update Registration.tsx

--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -39,7 +39,7 @@ const Registration: React.FC = () => {
     console.log("Payload being sent:", payload);
 
     try {
-      const response = await fetch("http://localhost:8080/register", {
+      const response = await fetch("https://defmogu.in/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
