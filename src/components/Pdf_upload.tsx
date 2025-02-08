14c144a0d875def5601c522fef523a8c8c941d86
Meghdeep77 <117975443+Meghdeep77@users.noreply.github.com>
2025-02-08 18:11:03 +0530

    Update Pdf_upload.tsx

--- src/components/Pdf_upload.tsx
+++ src/components/Pdf_upload.tsx
@@ -81,8 +81,8 @@ function Pdf_upload() {
 
     const endpoint =
       selectedFileType === "pdf"
-        ? `${apiUrl}/summarize_pdf`
-        : `${apiUrl}/summarize_ppt`;
+        ? `${apiUrl}/summarize_pdf/`
+        : `${apiUrl}/summarize_ppt/`;
 
     try {
       setLoading(true);
@@ -121,8 +121,8 @@ function Pdf_upload() {
 
     const endpoint =
       selectedFileType === "pdf"
-        ? `${apiUrl}/gen_ques_pdf`
-        : `${apiUrl}/gen_ques_ppt`;
+        ? `${apiUrl}/gen_ques_pdf/`
+        : `${apiUrl}/gen_ques_ppt/`;
 
     try {
       setLoading(true);
