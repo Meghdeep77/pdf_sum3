d615e59779f0d9f0bd6e7a27953fbf24a7ca6782
Meghdeep77 <117975443+Meghdeep77@users.noreply.github.com>
2025-02-08 10:23:23 +0530

    Update Pdf_upload.tsx

--- src/components/Pdf_upload.tsx
+++ src/components/Pdf_upload.tsx
@@ -81,8 +81,8 @@ function Pdf_upload() {
 
     const endpoint =
       selectedFileType === "pdf"
-        ? `${apiUrl}/summarize_pdf`
-        : `${apiUrl}/summarize_ppt`;
+        ? `https://defmogu.in/summarize_pdf`
+        : `https://defmogu.in/summarize_ppt`;
 
     try {
       setLoading(true);
@@ -121,8 +121,8 @@ function Pdf_upload() {
 
     const endpoint =
       selectedFileType === "pdf"
-        ? `${apiUrl}/gen_ques_pdf`
-        : `${apiUrl}/gen_ques_ppt`;
+        ? `https://defmogu.in/gen_ques_pdf`
+        : `https://defmogu.in/gen_ques_ppt`;
 
     try {
       setLoading(true);
